ec50cf2e44bd7f77021fbdb49914c3cfef5f1f26
Gepsmin <gepsmin@hotmail.com>
2020-08-27 12:17:15 +0300

    Avatar Animation Structure for headDiameter

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -25,7 +25,28 @@ class Avatar extends Component{
             outsideLeg: '90',
             insideLeg: '80',
             legDiameter: '18',
-            wrist: '7'
+            wrist: '7',
+
+            xround : "stop",
+            round : 0,
+            changing : '',
+
+            xheadDiameter: '16',
+            xheadLength: '24',
+            xneckLine: '12',
+            xneckLength: '13',
+            xshoulderLength: '42',
+            xchest: '32',
+            xwaist: '30',
+            xbodyLength: '48',
+            xarmDiameter: '10',
+            xhips: '36',
+            xarmLength: '54',
+            xhandDiameter: '6',
+            xoutsideLeg: '90',
+            xinsideLeg: '80',
+            xlegDiameter: '18',
+            xwrist: '7',
         }
 
         this.change = this.change.bind(this);
@@ -33,11 +54,86 @@ class Avatar extends Component{
         this.submitClick = this.submitClick.bind(this);
     }
 
+    changeAnimate(round, origin, master){
+        var value = 0;
+        var that = this;
+        if(this.state.xround === "continue"){
+            if(round==0){
+                value = origin*1
+            }else if(round==1){
+                value = master*1 + (origin*1 - master*1) /2
+            }else if(round==2){
+                value = master*1 + (origin*1 - master*1) /3
+            }else if(round==3){
+                value = master*1 + (origin*1 - master*1) /4
+            }else if(round==4){
+                value = master*1 + (origin*1 - master*1) /5
+            }else if(round==5){
+                value = master*1 + (origin*1 - master*1) /6
+            }else if(round==6){
+                value = master*1 + (origin*1 - master*1) /7
+            }else if(round==7){
+                value = master*1 + (origin*1 - master*1) /8
+            }else if(round==8){
+                value = master*1 + (origin*1 - master*1) /9
+            }else if(round==9){
+                value = master*1 + (origin*1 - master*1) /10
+            }else if(round==10){
+                value = master*1 + (origin*1 - master*1) /11
+            }else if(round==11){
+                value = master*1 + (origin*1 - master*1) /12
+            }else if(round==12){
+                value = master*1 + (origin*1 - master*1) /13
+            }else if(round==13){
+                value = master*1 + (origin*1 - master*1) /14
+            }else if(round==14){
+                value = master*1 + (origin*1 - master*1) /15
+            }else if(round==15){
+                value = master*1 + (origin*1 - master*1) /16
+            }else if(round==16){
+                value = master*1 + (origin*1 - master*1) /17
+            }else if(round==17){
+                value = master*1 + (origin*1 - master*1) /18
+            }else if(round==18){
+                value = master*1 + (origin*1 - master*1) /19
+            }else if(round==19){
+                value = master*1 + (origin*1 - master*1) /20
+            }else if(round==20){
+                value = master*1 + (origin*1 - master*1) /21
+            }else if(round==21){
+                value = master*1 + (origin*1 - master*1) /22
+            }else if(round==22){
+                value = master*1 + (origin*1 - master*1) /23
+            }else if(round==23){
+                value = master*1 + (origin*1 - master*1) /24
+            }
+        }else{
+            setTimeout(function(){that.setState(()=>({round:23, xround:"continue"}))},1000);
+        }
+        console.log("value: " , value)
+        if(round>=0&&round<24&&this.state.xround==="continue"){
+            setTimeout( function(){that.setState(()=>({xheadDiameter: value, round: round-1}))},15)
+        }else if(round==0){
+            this.setState(()=>({xround: "stop"}));
+        }
+        
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        if(prevState.round!=this.state.round){
+            console.log("updated and round changed: ", this.state.round);
+            this.changeAnimate(this.state.round, this.state.headDiameter, this.state.xheadDiameter);
+        }
+    }
+
     change(e){
         let value = e.target.value;
         let name = e.target.name;
+        if(value===0){
+            return;
+        }
         if(name==='headDiameter'){
-            this.setState(()=>({headDiameter: value}))
+            this.setState(()=>({headDiameter: value, round:21,xround: "stop"}))
         }else if(name==='headLength'){
             this.setState(()=>({headLength:value}))
         }else if(name==='neckLine'){
@@ -99,12 +195,28 @@ class Avatar extends Component{
             legDiameter: this.state.legDiameter,
             wrist: this.state.wrist
         }
-
+        console.log(this.state);
         this.props.addBody(obj)
     }
 
 
     render(){
+        var headDiameter = this.state.xheadDiameter;
+        var headLength = this.state.xheadLength;
+        var neckLine = this.state.xneckLine;
+        var neckLength = this.state.xneckLength;
+        var shoulderLength = this.state.xshoulderLength;
+        var chest = this.state.xchest;
+        var waist = this.state.xwaist;
+        var bodyLength = this.state.xbodyLength;
+        var armDiameter = this.state.xarmDiameter;
+        var hips = this.state.xhips;
+        var armLength = this.state.xarmLength;
+        var handDiameter = this.state.xhandDiameter;
+        var outsideLeg = this.state.outsideLeg;
+        var insideLeg = this.state.xinsideLeg;
+        var legDiameter = this.state.xlegDiameter;
+        var wrist = this.state.xwrist;
         return(
             <div className="AvatarPage">
                 <div className="AvatarButton" onClick={this.buttonClick}>Avatar Button</div>
@@ -244,7 +356,7 @@ class Avatar extends Component{
                             onBlur={this.blur}>
                             </input>
                         </div>
-                        
+                          
                         <div>
                             <input name='wrist'
                             className="input ex"
@@ -257,25 +369,25 @@ class Avatar extends Component{
                     <div className="Avatar2D">
                         <svg className="AvatarSVG">
                             <path fill="white" stroke="black" strokeWidth="1.5"
-                            d={`M ${20-this.state.neckLine*3/2} ${20+this.state.headLength*3-this.state.neckLength*3/2}
-                            L ${20-this.state.neckLine*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2}
-                            L ${20+this.state.neckLine*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2}
-                            L ${20+this.state.neckLine*3/2} ${20+this.state.headLength*3-this.state.neckLength*3/2}`}
+                            d={`M ${20-neckLine*3/2} ${20+headLength*3-neckLength*3/2}
+                            L ${20-neckLine*3/2} ${20+headLength*3+neckLength*3/2}
+                            L ${20+neckLine*3/2} ${20+headLength*3+neckLength*3/2}
+                            L ${20+neckLine*3/2} ${20+headLength*3-neckLength*3/2}`}
                             ></path> {/* neck */}
                             <path fill="white" stroke="black" strokeWidth="1.5"
                             d={`M 20 20
-                            Q ${20+this.state.headDiameter*3/2} 20 ${20+this.state.headDiameter*3/2} ${20+this.state.headLength*3/2}
-                            Q ${20+this.state.headDiameter*3/2} ${20+this.state.headLength*3} 20 ${20+this.state.headLength*3}
-                            Q ${20-this.state.headDiameter*3/2} ${20+this.state.headLength*3} ${20-this.state.headDiameter*3/2} ${20+this.state.headLength*3/2} 
-                            Q ${20-this.state.headDiameter*3/2} 20 20 20 Z`}
+                            Q ${20+headDiameter*3/2} 20 ${20+headDiameter*3/2} ${20+headLength*3/2}
+                            Q ${20+headDiameter*3/2} ${20+headLength*3} 20 ${20+headLength*3}
+                            Q ${20-headDiameter*3/2} ${20+headLength*3} ${20-headDiameter*3/2} ${20+headLength*3/2} 
+                            Q ${20-headDiameter*3/2} 20 20 20 Z`}
                             ></path> {/* head */}
 
                             <path fill="white" stroke="black" strokeWidth="1.5"
-                            d={`M ${20-this.state.shoulderLength*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2}
-                            L ${20+this.state.shoulderLength*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2}
-                            L ${20+this.state.chest*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.armDiameter*3}
-                            L ${20+this.state.waist*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*2}
-                            L ${20+this.state.hips*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*3}
+                            d={`M ${20-shoulderLength*3/2} ${20+headLength*3+neckLength*3/2}
+                            L ${20+shoulderLength*3/2} ${20+headLength*3+neckLength*3/2}
+                            L ${20+chest*3/2} ${20+headLength*3+neckLength*3/2+this.state.armDiameter*3}
+                            L ${20+waist*3/2} ${20+headLength*3+neckLength*3/2+this.state.bodyLength*2}
+                            L ${20+hips*3/2} ${20+headLength*3+neckLength*3/2+this.state.bodyLength*3}
 
                             L ${20-this.state.hips*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*3}
                             L ${20-this.state.waist*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*2}
