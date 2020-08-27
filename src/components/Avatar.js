0db80c75fc93324471956a9fd2d25a75e233cecd
Gepsmin <gepsmin@hotmail.com>
2020-08-27 13:51:50 +0300

    Avatar First Animation Done

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -112,9 +112,42 @@ class Avatar extends Component{
         }
         console.log("value: " , value)
         if(round>=0&&round<24&&this.state.xround==="continue"){
-            setTimeout( function(){that.setState(()=>({xheadDiameter: value, round: round-1}))},15)
+            const name = this.state.changing;
+            if(name==='headDiameter'){
+                setTimeout( function(){that.setState(()=>({xheadDiameter: value, round: round-1}))},15)
+            }else if(name==='headLength'){
+                setTimeout( function(){that.setState(()=>({xheadLength: value, round: round-1}))},15)
+            }else if(name==='neckLine'){
+                setTimeout( function(){that.setState(()=>({xneckLine: value, round: round-1}))},15)
+            }else if(name==='neckLength'){
+                setTimeout( function(){that.setState(()=>({xneckLength: value, round: round-1}))},15)
+            }else if(name==='shoulderLength'){
+                setTimeout( function(){that.setState(()=>({xshoulderLength: value, round: round-1}))},15)
+            }else if(name==='chest'){
+                setTimeout( function(){that.setState(()=>({xchest: value, round: round-1}))},15)
+            }else if(name==='waist'){
+                setTimeout( function(){that.setState(()=>({xwaist: value, round: round-1}))},15)
+            }else if(name==='bodyLength'){
+                setTimeout( function(){that.setState(()=>({xbodyLength: value, round: round-1}))},15)
+            }else if(name==='armDiameter'){
+                setTimeout( function(){that.setState(()=>({xarmDiameter: value, round: round-1}))},15)
+            }else if(name==='hips'){
+                setTimeout( function(){that.setState(()=>({xhips: value, round: round-1}))},15)
+            }else if(name==='armLength'){
+                setTimeout( function(){that.setState(()=>({xarmLength: value, round: round-1}))},15)
+            }else if(name==='handDiameter'){
+                setTimeout( function(){that.setState(()=>({xhandDiameter: value, round: round-1}))},15)
+            }else if(name==='outsideLeg'){
+                setTimeout( function(){that.setState(()=>({xoutsideLeg: value, round: round-1}))},15)
+            }else if(name==='insideLeg'){
+                setTimeout( function(){that.setState(()=>({xinsideLeg: value, round: round-1}))},15)
+            }else if(name==='legDiameter'){
+                setTimeout( function(){that.setState(()=>({xlegDiameter: value, round: round-1}))},15)
+            }else if(name==='wrist'){
+                setTimeout( function(){that.setState(()=>({xwrist: value, round: round-1}))},15)
+            }
         }else if(round==0){
-            this.setState(()=>({xround: "stop"}));
+            this.setState(()=>({xround: "stop", changing: ""}));
         }
         
     }
@@ -122,7 +155,41 @@ class Avatar extends Component{
     componentDidUpdate(prevProps, prevState){
         if(prevState.round!=this.state.round){
             console.log("updated and round changed: ", this.state.round);
-            this.changeAnimate(this.state.round, this.state.headDiameter, this.state.xheadDiameter);
+            const name = this.state.changing;
+            
+            if(name==='headDiameter'){
+                this.changeAnimate(this.state.round, this.state.headDiameter, this.state.xheadDiameter);
+            }else if(name==='headLength'){
+                this.changeAnimate(this.state.round, this.state.headLength, this.state.xheadLength);
+            }else if(name==='neckLine'){
+                this.changeAnimate(this.state.round, this.state.neckLine, this.state.xneckLine);
+            }else if(name==='neckLength'){
+                this.changeAnimate(this.state.round, this.state.neckLength, this.state.xneckLength);
+            }else if(name==='shoulderLength'){
+                this.changeAnimate(this.state.round, this.state.shoulderLength, this.state.xshoulderLength);
+            }else if(name==='chest'){
+                this.changeAnimate(this.state.round, this.state.chest, this.state.xchest);
+            }else if(name==='waist'){
+                this.changeAnimate(this.state.round, this.state.waist, this.state.xwaist);
+            }else if(name==='bodyLength'){
+                this.changeAnimate(this.state.round, this.state.bodyLength, this.state.xbodyLength);
+            }else if(name==='armDiameter'){
+                this.changeAnimate(this.state.round, this.state.armDiameter, this.state.xarmDiameter);
+            }else if(name==='hips'){
+                this.changeAnimate(this.state.round, this.state.hips, this.state.xhips);
+            }else if(name==='armLength'){
+                this.changeAnimate(this.state.round, this.state.armLength, this.state.xarmLength);
+            }else if(name==='handDiameter'){
+                this.changeAnimate(this.state.round, this.state.handDiameter, this.state.xhandDiameter);
+            }else if(name==='outsideLeg'){
+                this.changeAnimate(this.state.round, this.state.outsideLeg, this.state.xoutsideLeg);
+            }else if(name==='insideLeg'){
+                this.changeAnimate(this.state.round, this.state.insideLeg, this.state.xinsideLeg);
+            }else if(name==='legDiameter'){
+                this.changeAnimate(this.state.round, this.state.legDiameter, this.state.xlegDiameter);
+            }else if(name==='wrist'){
+                this.changeAnimate(this.state.round, this.state.wrist, this.state.xwrist);
+            }
         }
     }
 
@@ -133,37 +200,37 @@ class Avatar extends Component{
             return;
         }
         if(name==='headDiameter'){
-            this.setState(()=>({headDiameter: value, round:21,xround: "stop"}))
+            this.setState(()=>({headDiameter: value, round:21,xround: "stop", changing: "headDiameter"}))
         }else if(name==='headLength'){
-            this.setState(()=>({headLength:value}))
+            this.setState(()=>({headLength:value, round:21,xround: "stop", changing: "headLength"}))
         }else if(name==='neckLine'){
-            this.setState(()=>({neckLine:value}))
+            this.setState(()=>({neckLine:value, round:21,xround: "stop", changing: "neckLine"}))
         }else if(name==='neckLength'){
-            this.setState(()=>({neckLength:value}))
+            this.setState(()=>({neckLength:value, round:21,xround: "stop", changing: "neckLength"}))
         }else if(name==='shoulderLength'){
-            this.setState(()=>({shoulderLength:value}))
+            this.setState(()=>({shoulderLength:value, round:21,xround: "stop", changing: "shoulderLength"}))
         }else if(name==='chest'){
-            this.setState(()=>({chest:value}))
+            this.setState(()=>({chest:value, round:21,xround: "stop", changing: "chest"}))
         }else if(name==='waist'){
-            this.setState(()=>({waist:value}))
+            this.setState(()=>({waist:value, round:21,xround: "stop", changing: "waist"}))
         }else if(name==='bodyLength'){
-            this.setState(()=>({bodyLength:value}))
+            this.setState(()=>({bodyLength:value, round:21,xround: "stop", changing: "bodyLength"}))
         }else if(name==='armDiameter'){
-            this.setState(()=>({armDiameter:value}))
+            this.setState(()=>({armDiameter:value, round:21,xround: "stop", changing: "armDiameter"}))
         }else if(name==='hips'){
-            this.setState(()=>({hips:value}))
+            this.setState(()=>({hips:value, round:21,xround: "stop", changing: "hips"}))
         }else if(name==='armLength'){
-            this.setState(()=>({armLength:value}))
+            this.setState(()=>({armLength:value, round:21,xround: "stop", changing: "armLength"}))
         }else if(name==='handDiameter'){
-            this.setState(()=>({handDiameter:value}))
+            this.setState(()=>({handDiameter:value, round:21,xround: "stop", changing: "handDiameter"}))
         }else if(name==='outsideLeg'){
-            this.setState(()=>({outsideLeg:value}))
+            this.setState(()=>({outsideLeg:value, round:21,xround: "stop", changing: "outsideLeg"}))
         }else if(name==='insideLeg'){
-            this.setState(()=>({insideLeg:value}))
+            this.setState(()=>({insideLeg:value, round:21,xround: "stop", changing: "insideLeg"}))
         }else if(name==='legDiameter'){
-            this.setState(()=>({legDiameter:value}))
+            this.setState(()=>({legDiameter:value, round:21,xround: "stop", changing: "legDiameter"}))
         }else if(name==='wrist'){
-            this.setState(()=>({wrist:value}))
+            this.setState(()=>({wrist:value, round:21,xround: "stop", changing: "wrist"}))
         }
     }
 
@@ -385,52 +452,52 @@ class Avatar extends Component{
                             <path fill="white" stroke="black" strokeWidth="1.5"
                             d={`M ${20-shoulderLength*3/2} ${20+headLength*3+neckLength*3/2}
                             L ${20+shoulderLength*3/2} ${20+headLength*3+neckLength*3/2}
-                            L ${20+chest*3/2} ${20+headLength*3+neckLength*3/2+this.state.armDiameter*3}
-                            L ${20+waist*3/2} ${20+headLength*3+neckLength*3/2+this.state.bodyLength*2}
-                            L ${20+hips*3/2} ${20+headLength*3+neckLength*3/2+this.state.bodyLength*3}
+                            L ${20+chest*3/2} ${20+headLength*3+neckLength*3/2+armDiameter*3}
+                            L ${20+waist*3/2} ${20+headLength*3+neckLength*3/2+bodyLength*2}
+                            L ${20+hips*3/2} ${20+headLength*3+neckLength*3/2+bodyLength*3}
 
-                            L ${20-this.state.hips*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*3}
-                            L ${20-this.state.waist*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*2}
-                            L ${20-this.state.chest*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.armDiameter*3}
-                            L ${20-this.state.shoulderLength*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2} Z`}
+                            L ${20-hips*3/2} ${20+headLength*3+neckLength*3/2+bodyLength*3}
+                            L ${20-waist*3/2} ${20+headLength*3+neckLength*3/2+bodyLength*2}
+                            L ${20-chest*3/2} ${20+headLength*3+neckLength*3/2+armDiameter*3}
+                            L ${20-shoulderLength*3/2} ${20+headLength*3+neckLength*3/2} Z`}
                             ></path> {/* up body */}
 
                             <path  fill="white" stroke="black" strokeWidth="1.5"
-                            d={`M ${20-this.state.shoulderLength*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2}
-                            L ${20-this.state.shoulderLength*3/2-this.state.armLength*3/4} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.armLength*3/2}
-                            L ${20-this.state.shoulderLength*3/2-this.state.armLength*3/4} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.armLength*3}
-                            L ${20-this.state.shoulderLength*3/2-this.state.armLength*3/4+this.state.handDiameter*3} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.armLength*3}
-                            L ${20-this.state.shoulderLength*3/2-this.state.armLength*3/4+(this.state.handDiameter*1+this.state.armDiameter*1)*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.armLength*3/2+this.state.armDiameter*1}
-                            L ${20-this.state.chest*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.armDiameter*3} 
-                            L ${20-this.state.shoulderLength*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2} Z`}
+                            d={`M ${20-shoulderLength*3/2} ${20+headLength*3+neckLength*3/2}
+                            L ${20-shoulderLength*3/2-armLength*3/4} ${20+headLength*3+neckLength*3/2+armLength*3/2}
+                            L ${20-shoulderLength*3/2-armLength*3/4} ${20+headLength*3+neckLength*3/2+armLength*3}
+                            L ${20-shoulderLength*3/2-armLength*3/4+handDiameter*3} ${20+headLength*3+neckLength*3/2+armLength*3}
+                            L ${20-shoulderLength*3/2-armLength*3/4+(handDiameter*1+armDiameter*1)*3/2} ${20+headLength*3+neckLength*3/2+armLength*3/2+armDiameter*1}
+                            L ${20-chest*3/2} ${20+headLength*3+neckLength*3/2+armDiameter*3} 
+                            L ${20-shoulderLength*3/2} ${20+headLength*3+neckLength*3/2} Z`}
                             ></path> {/* left arm */}
 
                             <path  fill="white" stroke="black" strokeWidth="1.5"
-                            d={`M ${20+this.state.shoulderLength*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2}
-                            L ${20+this.state.shoulderLength*3/2+this.state.armLength*3/4} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.armLength*3/2}
-                            L ${20+this.state.shoulderLength*3/2+this.state.armLength*3/4} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.armLength*3}
-                            L ${20+this.state.shoulderLength*3/2+this.state.armLength*3/4-this.state.handDiameter*3} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.armLength*3}
-                            L ${20+this.state.shoulderLength*3/2+this.state.armLength*3/4-(this.state.handDiameter*1+this.state.armDiameter*1)*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.armLength*3/2+this.state.armDiameter*1}
-                            L ${20+this.state.chest*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.armDiameter*3} 
-                            L ${20+this.state.shoulderLength*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2} Z`}
+                            d={`M ${20+shoulderLength*3/2} ${20+headLength*3+neckLength*3/2}
+                            L ${20+shoulderLength*3/2+armLength*3/4} ${20+headLength*3+neckLength*3/2+armLength*3/2}
+                            L ${20+shoulderLength*3/2+armLength*3/4} ${20+headLength*3+neckLength*3/2+armLength*3}
+                            L ${20+shoulderLength*3/2+armLength*3/4-handDiameter*3} ${20+headLength*3+neckLength*3/2+armLength*3}
+                            L ${20+shoulderLength*3/2+armLength*3/4-(handDiameter*1+armDiameter*1)*3/2} ${20+headLength*3+neckLength*3/2+armLength*3/2+armDiameter*1}
+                            L ${20+chest*3/2} ${20+headLength*3+neckLength*3/2+armDiameter*3} 
+                            L ${20+shoulderLength*3/2} ${20+headLength*3+neckLength*3/2} Z`}
                             ></path> {/* right arm */}
 
                             <path  fill="white" stroke="black" strokeWidth="1.5"
-                            d={`M ${20-this.state.hips*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*3}
-                            L ${20-this.state.legDiameter*3} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*3+(this.state.outsideLeg*1-this.state.insideLeg*1)*3}
-                            L ${20-(this.state.legDiameter*1+this.state.wrist*1)*3/2-this.state.wrist*3} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*3+(this.state.outsideLeg*1-this.state.insideLeg*1)*3+this.state.insideLeg*3/2}
-                            L ${20-this.state.wrist*6} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*3+(this.state.outsideLeg*1-this.state.insideLeg*1)*3+this.state.insideLeg*3}
-                            L ${20-this.state.wrist*3} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*3+(this.state.outsideLeg*1-this.state.insideLeg*1)*3+this.state.insideLeg*3}
-                            L ${20-this.state.wrist*3} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*3+(this.state.outsideLeg*1-this.state.insideLeg*1)*3+this.state.insideLeg*3/2}
+                            d={`M ${20-hips*3/2} ${20+headLength*3+neckLength*3/2+bodyLength*3}
+                            L ${20-legDiameter*3} ${20+headLength*3+neckLength*3/2+bodyLength*3+(outsideLeg*1-insideLeg*1)*3}
+                            L ${20-(legDiameter*1+wrist*1)*3/2-wrist*3} ${20+headLength*3+neckLength*3/2+bodyLength*3+(outsideLeg*1-insideLeg*1)*3+insideLeg*3/2}
+                            L ${20-wrist*6} ${20+headLength*3+neckLength*3/2+bodyLength*3+(outsideLeg*1-insideLeg*1)*3+insideLeg*3}
+                            L ${20-wrist*3} ${20+headLength*3+neckLength*3/2+bodyLength*3+(outsideLeg*1-insideLeg*1)*3+insideLeg*3}
+                            L ${20-wrist*3} ${20+headLength*3+neckLength*3/2+bodyLength*3+(outsideLeg*1-insideLeg*1)*3+insideLeg*3/2}
                             
-                            L ${20} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*3+(this.state.outsideLeg*1-this.state.insideLeg*1)*3}
+                            L ${20} ${20+headLength*3+neckLength*3/2+bodyLength*3+(outsideLeg*1-insideLeg*1)*3}
                             
-                            L ${20+this.state.wrist*3} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*3+(this.state.outsideLeg*1-this.state.insideLeg*1)*3+this.state.insideLeg*3/2}
-                            L ${20+this.state.wrist*3} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*3+(this.state.outsideLeg*1-this.state.insideLeg*1)*3+this.state.insideLeg*3}
-                            L ${20+this.state.wrist*6} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*3+(this.state.outsideLeg*1-this.state.insideLeg*1)*3+this.state.insideLeg*3} 
-                            L ${20+(this.state.legDiameter*1+this.state.wrist*1)*3/2+this.state.wrist*3} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*3+(this.state.outsideLeg*1-this.state.insideLeg*1)*3+this.state.insideLeg*3/2}
-                            L ${20+this.state.legDiameter*3} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*3+(this.state.outsideLeg*1-this.state.insideLeg*1)*3}
-                            L ${20+this.state.hips*3/2} ${20+this.state.headLength*3+this.state.neckLength*3/2+this.state.bodyLength*3} Z
+                            L ${20+wrist*3} ${20+headLength*3+neckLength*3/2+bodyLength*3+(outsideLeg*1-insideLeg*1)*3+insideLeg*3/2}
+                            L ${20+wrist*3} ${20+headLength*3+neckLength*3/2+bodyLength*3+(outsideLeg*1-insideLeg*1)*3+insideLeg*3}
+                            L ${20+wrist*6} ${20+headLength*3+neckLength*3/2+bodyLength*3+(outsideLeg*1-insideLeg*1)*3+insideLeg*3} 
+                            L ${20+(legDiameter*1+wrist*1)*3/2+wrist*3} ${20+headLength*3+neckLength*3/2+bodyLength*3+(outsideLeg*1-insideLeg*1)*3+insideLeg*3/2}
+                            L ${20+legDiameter*3} ${20+headLength*3+neckLength*3/2+bodyLength*3+(outsideLeg*1-insideLeg*1)*3}
+                            L ${20+hips*3/2} ${20+headLength*3+neckLength*3/2+bodyLength*3} Z
                             `}
                             ></path> {/* bottom */}
 
